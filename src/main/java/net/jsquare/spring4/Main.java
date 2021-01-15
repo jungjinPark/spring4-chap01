ef1536bc3ba27dc8d622446726a79f7b2fb58c11
jungjinPark <jjpark001@gmail.com>
2021-01-16 05:12:00 +0900

    Initial commit

---
+++ src/main/java/net/jsquare/spring4/Main.java
@@ -0,0 +1,22 @@
+package net.jsquare.spring4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MavenBuildRunner buildRunner = new MavenBuildRunner();
+        buildRunner.setMavenPath("c:\\apache-maven-3.1.1");
+
+        Project sampleProject = new Project();
+        List<String> srcDirs = new ArrayList<>();
+        srcDirs.add("src");
+        srcDirs.add("srcResources");
+        sampleProject.setSrcDirs(srcDirs);
+        sampleProject.setBinDir("bin");
+        sampleProject.setBuildRunner(buildRunner);
+
+        sampleProject.build();
+
+    }
+}
